a2d5612a00024306ccc16fcc937a04dea1d2b279
Rishav Singh <rishavrajcrj@gmail.com>
2023-04-21 22:27:49 +0530

    Update userMetaSchema.js

--- models/userMetaSchema.js
+++ models/userMetaSchema.js
@@ -2,13 +2,9 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 
 const userMetaSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
+  userID: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Users",
   },
   podcast: [
     {
