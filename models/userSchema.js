c07b568b0e2dc6e4b1ba371868d5a77409dee032
Rishav Singh <rishavrajcrj@gmail.com>
2023-04-21 22:06:54 +0530

    Update userSchema.js

--- models/userSchema.js
+++ models/userSchema.js
@@ -43,6 +43,10 @@ const userSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: "UserMeta",
   },
+  createdPodcast: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Podcast",
+  },
   date: {
     type: Date,
     default: Date.now,
