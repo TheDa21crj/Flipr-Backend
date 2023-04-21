c17cd73562fce094954a7be61e82b5c5c88d5a4a
Rishav Singh <rishavrajcrj@gmail.com>
2023-04-21 19:50:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(express.json({ extended: false }));
 app.use(cookieParser());
 
 // routes
-// app.use("/api/user", require("./routes/user"));
+app.use("/api/user", require("./routes/user"));
 // app.use("/api/attendance", require("./routes/save"));
 
 // test route
