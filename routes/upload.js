b5c1d94937716e68d53c80de3ba1b8e71ccdeb73
Rishav Singh <rishavrajcrj@gmail.com>
2023-04-22 00:57:08 +0530

    Update upload.js

--- routes/upload.js
+++ routes/upload.js
@@ -1,8 +1,38 @@
 const express = require("express");
-const router = express.Router();
-const loginController = require("../controllers/login/loginController");
-const auth = require("../middleWare/auth");
-const { check, validationResult } = require("express-validator");
+const mediaController = require("./../controllers/mediaController");
+const multer = require("multer");
+const fs = require("fs");
+const path = require("path");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    if (!fs.existsSync("public")) {
+      fs.mkdirSync("public");
+    }
+
+    if (!fs.existsSync("public/videos")) {
+      fs.mkdirSync("public/videos");
+    }
+
+    cb(null, "public/videos");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname);
+  },
+});
+
+const upload = multer({
+  storage: storage,
+  fileFilter: function (req, file, cb) {
+    var ext = path.extname(file.originalname);
+
+    if (ext !== ".mkv" && ext !== ".mp4") {
+      return cb(new Error("Only videos are allowed!"));
+    }
+
+    cb(null, true);
+  },
+});
 
 // auth
 router.use(auth);
