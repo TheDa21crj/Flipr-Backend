043b50db96c7ef9fcaa3fb980e9da5dcd7160595
Rishav Singh <rishavrajcrj@gmail.com>
2023-04-21 19:20:35 +0530

    Create db.js

---
+++ config/db.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    console.log(process.env.mongoURL);
+    await mongoose.connect(process.env.mongoURL, {
+      useNewUrlParser: true,
+    });
+    console.log("DataBase Connected");
+  } catch (error) {
+    console.log("Error");
+    console.log(error);
+  }
+};
+
+module.exports = connectDB;
