c921d700f43c0e939cb02c80c972790205d7bab7
Rishav Singh <rishavrajcrj@gmail.com>
2023-04-22 02:55:57 +0530

    Update podcastController.js

--- controllers/podcast/podcastController.js
+++ controllers/podcast/podcastController.js
@@ -1,11 +1,24 @@
-const HttpError = require("../../models/HttpError");
 const user = require("../../models/userSchema");
-const jwt = require("jsonwebtoken");
+const HttpError = require("../../models/HttpError");
+const podcast = require("../../models/podcastSchema");
 const { check, validationResult } = require("express-validator");
-const nodemailer = require("nodemailer");
 
 // Private || Create Podcast
-const createPodcast = async (req, res, next) => {};
+const createPodcast = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
 
-exports.login = login;
-exports.register = register;
+  const { title, des, thumbnail, year, tag, category } = req.body;
+
+  try {
+    console.log(title, des, thumbnail, year, tag, category);
+  } catch (err) {
+    console.log(err);
+    const error = new HttpError("Cannot add user", 400);
+    return next(error);
+  }
+};
+
+exports.createPodcast = createPodcast;
