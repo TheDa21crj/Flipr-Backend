92c7ed83b194899d586efd5085791b3e26b0ea9f
Rishav Singh <rishavrajcrj@gmail.com>
2023-04-21 22:03:09 +0530

    Update podcast.js

--- models/podcast.js
+++ models/podcast.js
@@ -2,35 +2,20 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 
 const podcastSchema = new mongoose.Schema({
-  name: {
+  title: {
     type: String,
     required: true,
   },
-  email: {
+  des: {
     type: String,
     required: true,
   },
-  podcast: [
+  author: [
     {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "Podcast",
-      subscribed: {
-        type: "Boolean",
-        default: false,
-      },
-      watching: {
-        type: "string",
-      },
-      rating: {
-        type: "Number",
-        default: 0,
-      },
+      ref: "Users",
     },
   ],
-  password: {
-    type: "string",
-    required: true,
-  },
   avatar: {
     type: "string",
     required: true,
