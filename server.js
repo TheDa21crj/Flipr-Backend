8262134cc7669897f8ff374e5d0e1d69d10b99e3
Rishav Singh <rishavrajcrj@gmail.com>
2023-04-22 08:44:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -34,6 +34,7 @@ app.use(cookieParser());
 // routes
 app.use("/api/user", require("./routes/user"));
 app.use("/api/podcast", require("./routes/podcast"));
+app.use("/api/episode", require("./routes/episode"));
 app.use("/api/media", require("./routes/media"));
 
 app.use("/public", express.static(path.join(__dirname, "public")));
