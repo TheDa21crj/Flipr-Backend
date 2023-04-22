7fd463abe1936eb9d8cb4128155dbf89e6cc2b5e
Rishav Singh <rishavrajcrj@gmail.com>
2023-04-22 08:42:42 +0530

    Update episode.js

---
+++ routes/episode.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const auth = require("../middleWare/auth");
+const { check, validationResult } = require("express-validator");
+const podcastController = require("../controllers/podcast/podcastController");
+
+const router = express.Router();
+
+// auth
+router.use(auth);
+
+// Private || Create Podcast
+router.post(
+  "/create",
+  [check("title", "title is Required").not().isEmpty()],
+  [check("des", "des is Required").not().isEmpty()],
+  [check("thumbnail", "thumbnail is Required").not().isEmpty()],
+  [check("year", "year is Required").not().isEmpty()],
+  [check("tag", "tag is Required").not().isEmpty()],
+  [check("category", "category is Required").not().isEmpty()],
+  podcastController.createPodcast
+);
+
+module.exports = router;
