8616d91c1c9b792c2f9b84bdc92f827b8491c911
Rishav Singh <rishavrajcrj@gmail.com>
2023-04-22 09:07:54 +0530

    Update episode.js

--- routes/episode.js
+++ routes/episode.js
@@ -2,8 +2,39 @@ const express = require("express");
 const auth = require("../middleWare/auth");
 const { check, validationResult } = require("express-validator");
 const episodeController = require("../controllers/episode/episodeController");
-
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
 const router = express.Router();
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    if (!fs.existsSync("public")) {
+      fs.mkdirSync("public");
+    }
+
+    if (!fs.existsSync("public/videos")) {
+      fs.mkdirSync("public/videos");
+    }
+
+    cb(null, "public/videos");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname);
+  },
+});
+
+const upload = multer({
+  storage: storage,
+  fileFilter: function (req, file, cb) {
+    var ext = path.extname(file.originalname);
+
+    if (ext !== ".mkv" && ext !== ".mp4" && ext !== ".mp3") {
+      return cb(new Error("Only videos and audio are allowed!"));
+    }
+    cb(null, true);
+  },
+});
 
 // auth
 router.use(auth);
@@ -16,6 +47,12 @@ router.post(
   [check("thumbnail", "thumbnail is Required").not().isEmpty()],
   [check("podcast", "podcast is Required").not().isEmpty()],
   [check("season", "season is Required").not().isEmpty()],
+  upload.fields([
+    {
+      name: "videos",
+      maxCount: 1,
+    },
+  ]),
   episodeController.createPodcast
 );
 
