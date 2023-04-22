5e3427143380360caa10306ed05ad0004fb4d2d2
Rishav Singh <rishavrajcrj@gmail.com>
2023-04-22 15:30:54 +0530

    Update episode.js

--- routes/episode.js
+++ routes/episode.js
@@ -24,12 +24,41 @@ const storage = multer.diskStorage({
   },
 });
 
+const storageImg = multer.diskStorage({
+  destination: function (req, file, cb) {
+    if (!fs.existsSync("public")) {
+      fs.mkdirSync("public");
+    }
+
+    if (!fs.existsSync("public/episode")) {
+      fs.mkdirSync("public/episode");
+    }
+
+    cb(null, "public/episode");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname);
+  },
+});
+
 const upload = multer({
   storage: storage,
   fileFilter: function (req, file, cb) {
     var ext = path.extname(file.originalname);
 
     if (ext !== ".mkv" && ext !== ".mp4" && ext !== ".mp3" && ext !== ".mov") {
+      return cb(new Error("Only videos and audio are allowed!"));
+    }
+    cb(null, true);
+  },
+});
+
+const uploads = multer({
+  storage: storageImg,
+  fileFilter: function (req, file, cb) {
+    var ext = path.extname(file.originalname);
+
+    if (ext != ".png" && ext != ".jpeg") {
       return cb(new Error("Only videos and audio are allowed!"));
     }
     cb(null, true);
@@ -50,6 +79,8 @@ router.post(
       name: "videos",
       maxCount: 1,
     },
+  ]),
+  uploads.fields([
     {
       name: "thumbnail",
       maxCount: 1,
