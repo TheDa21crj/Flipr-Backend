6ec1402e2cc6980492bff0606e6b7f4ea3d2e264
Rishav Singh <rishavrajcrj@gmail.com>
2023-04-22 12:23:02 +0530

    Update podcast.js

--- routes/podcast.js
+++ routes/podcast.js
@@ -1,9 +1,41 @@
+const fs = require("fs");
+const path = require("path");
+const multer = require("multer");
 const express = require("express");
 const auth = require("../middleWare/auth");
 const { check } = require("express-validator");
 const podcastController = require("../controllers/podcast/podcastController");
 
 const router = express.Router();
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    if (!fs.existsSync("public")) {
+      fs.mkdirSync("public");
+    }
+
+    if (!fs.existsSync("public/videos")) {
+      fs.mkdirSync("public/videos");
+    }
+
+    cb(null, "public/videos");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname);
+  },
+});
+
+const upload = multer({
+  storage: storage,
+  fileFilter: function (req, file, cb) {
+    var ext = path.extname(file.originalname);
+
+    if (ext !== ".mkv" && ext !== ".mp4" && ext !== ".mp3") {
+      return cb(new Error("Only videos and audio are allowed!"));
+    }
+    cb(null, true);
+  },
+});
 
 // Public || Get Podcast by ID
 router.get(
