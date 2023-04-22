106bfa1e3634d3418ca4df40e87dd146601dff9e
Rishav Singh <rishavrajcrj@gmail.com>
2023-04-22 22:49:48 +0530

    Update episode.js

--- routes/episode.js
+++ routes/episode.js
@@ -25,42 +25,12 @@ const storage = multer.diskStorage({
   },
 });
 
-const storageImg = multer.diskStorage({
-  destination: function (req, file, cb) {
-    if (!fs.existsSync("public")) {
-      fs.mkdirSync("public");
-    }
-
-    if (!fs.existsSync("public/episode")) {
-      fs.mkdirSync("public/episode");
-    }
-
-    // cb(null, "public/episode");
-    cb(null, path.resolve(__dirname, "public/episode"));
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + file.originalname);
-  },
-});
-
 const upload = multer({
   storage: storage,
   fileFilter: function (req, file, cb) {
     var ext = path.extname(file.originalname);
 
     if (ext !== ".mkv" && ext !== ".mp4" && ext !== ".mp3" && ext !== ".mov") {
-      return cb(new Error("Only videos and audio are allowed!"));
-    }
-    cb(null, true);
-  },
-});
-
-const uploads = multer({
-  storage: storageImg,
-  fileFilter: function (req, file, cb) {
-    var ext = path.extname(file.originalname);
-
-    if (ext != ".png" && ext != ".jpeg") {
       return cb(new Error("Only videos and audio are allowed!"));
     }
     cb(null, true);
@@ -81,8 +51,12 @@ router.post(
       name: "videos",
       maxCount: 1,
     },
+    {
+      name: "thumbnail",
+      maxCount: 1,
+    },
   ]),
-  
+
   // uploads.fields([
   //   {
   //     name: "thumbnail",
