6913fa13149f1eb0890f548d2d2fd98e9640c565
Rishav Singh <rishavrajcrj@gmail.com>
2023-04-24 11:10:48 +0530

    Update searchController.js

--- controllers/search/searchController.js
+++ controllers/search/searchController.js
@@ -5,9 +5,19 @@ const { check, validationResult } = require("express-validator");
 
 //Public || Podcast by Podcast ID
 const searchData = async (req, res, next) => {
-  const { query } = req.body;
+  const { query } = req.query.q;
 
   try {
+    // Search the database for matching records
+    podcast.find({ $text: { $search: query } }, function (err, records) {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ error: "Internal Server Error" });
+      }
+
+      // Return the matching records
+      res.json(records);
+    });
   } catch (err) {
     console.log(err);
     const error = new HttpError("Server Error", 400);
