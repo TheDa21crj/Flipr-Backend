ac01dc1b72620e038627ed42ccf34660fe773887
Rishav Singh <rishavrajcrj@gmail.com>
2023-04-24 10:56:55 +0530

    Update searchController.js

--- controllers/search/searchController.js
+++ controllers/search/searchController.js
@@ -4,31 +4,20 @@ const podcast = require("../../models/podcastSchema");
 const { check, validationResult } = require("express-validator");
 
 //Public || Podcast by Podcast ID
-const podcastbyID = async (req, res, next) => {
+const searchData = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
 
-  const { id } = req.body;
+  const { query } = req.body;
 
   try {
-    let podcastID = await podcast
-      .findOne({ _id: id })
-      .populate("episodes")
-      .populate("author", "_id name avatar bio");
-
-    if (podcastID) {
-      console.log(podcastID);
-      return res.status(202).json(podcastID);
-    } else {
-      return res.status(304).json("Does Not Exists");
-    }
   } catch (err) {
     console.log(err);
-    const error = new HttpError("Error", 400);
+    const error = new HttpError("Server Error", 400);
     return next(error);
   }
 };
 
-exports.podcastbyID = podcastbyID;
+exports.searchData = searchData;
