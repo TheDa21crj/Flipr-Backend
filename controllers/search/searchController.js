2a4eed595ad769255f91949e34791b65d2ae7672
Rishav Singh <rishavrajcrj@gmail.com>
2023-04-24 13:36:18 +0530

    Update searchController.js

--- controllers/search/searchController.js
+++ controllers/search/searchController.js
@@ -22,12 +22,18 @@ const searchData = async (req, res, next) => {
 //Public || Podcast by Podcast ID
 const searchLetterData = async (req, res, next) => {
   const query = req.query.q;
+  const firstLetter = query.charAt(0);
 
   console.log(query);
 
   try {
-    const records = await podcast.find({ $text: { $search: query } });
-    res.status(202).json(records);
+    const records = await podcast.find({
+      title: { $regex: "^" + firstLetter },
+    });
+    const filteredRecords = records.filter((record) =>
+      record.title.toLowerCase().includes(query.toLowerCase())
+    );
+    res.json(filteredRecords);
   } catch (err) {
     console.log(err);
     const error = new HttpError("Server Error", 400);
