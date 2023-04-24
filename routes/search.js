605b908c65c7e8488829d326df86a57e3015f45b
Rishav Singh <rishavrajcrj@gmail.com>
2023-04-24 10:19:32 +0530

    Update search.js

--- routes/search.js
+++ routes/search.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const searchController = require("../controllers/search/searchController");
-const auth = require("../middleWare/auth");
 const { check } = require("express-validator");
 
 const router = express.Router();
+
+// Search
+router.get("/subData", loginController.subData);
