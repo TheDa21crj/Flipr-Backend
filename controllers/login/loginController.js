53767dacb5eb3f9c362e7cf34c917d385ea2ff7d
Rishav Singh <rishavrajcrj@gmail.com>
2023-04-24 21:49:15 +0530

    Update loginController.js

--- controllers/login/loginController.js
+++ controllers/login/loginController.js
@@ -232,6 +232,8 @@ const subscribeID = async (req, res, next) => {
 
 // Private || Remove subscribe
 const unSubscribe = async (req, res, next) => {
+  console.log("___________________________________");
+
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
