eef1db24f04cc7cc76ad6c461ecaa2ddf7c986d4
Rishav Singh <rishavrajcrj@gmail.com>
2023-04-24 11:04:39 +0530

    Update searchController.js

--- controllers/search/searchController.js
+++ controllers/search/searchController.js
@@ -5,11 +5,6 @@ const { check, validationResult } = require("express-validator");
 
 //Public || Podcast by Podcast ID
 const searchData = async (req, res, next) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
-
   const { query } = req.body;
 
   try {
