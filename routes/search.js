69f433a78b92bb78e76ea36a8a969cced727c146
Rishav Singh <rishavrajcrj@gmail.com>
2023-04-24 10:53:33 +0530

    Update search.js

--- routes/search.js
+++ routes/search.js
@@ -5,4 +5,10 @@ const { check } = require("express-validator");
 const router = express.Router();
 
 // Search
-router.get("/subData", loginController.subData);
+router.get(
+  "/subData",
+  [check("query", "query is Required").not().isEmpty()],
+  searchController.subData
+);
+
+module.exports = router;
