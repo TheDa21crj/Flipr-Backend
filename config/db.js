c95ebfb459f6711fd498aaa7c0752f8188d2235c
Rishav Singh <rishavrajcrj@gmail.com>
2023-04-24 22:04:16 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,9 +3,12 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.mongoURL, {
-      useNewUrlParser: true,
-    });
+    await mongoose.connect(
+      "mongodb+srv://rishavrajcrj:LwiNgEuISXBZVbfL@cluster0.upi0axf.mongodb.net/?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+      }
+    );
     console.log("DataBase Connected");
   } catch (error) {
     console.log("Error");
