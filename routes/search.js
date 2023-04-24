d1b57ba96c3c3a06b370d84597c2df4fd8d6ab20
Rishav Singh <rishavrajcrj@gmail.com>
2023-04-24 11:42:02 +0530

    Update search.js

--- routes/search.js
+++ routes/search.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const searchController = require("../controllers/search/searchController");
+const auth = require("./../middleWare/auth");
 // const { check } = require("express-validator");
 
 const router = express.Router();
 
+// auth
+router.use(auth);
+
 // Search
 router.get("/", searchController.searchData);
+
+// Search
+router.get("/leeterwise", searchController.searchData);
 
 module.exports = router;
